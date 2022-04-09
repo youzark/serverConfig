59d07b9c979dcb0ee13ac24ededc46d32d8e4fc3
youzark <gacforstudy@gmail.com>
2022-04-09 15:51:15 +0800

    modify zsh config

--- zsh_server/ubuntu.sh
+++ zsh_server/ubuntu.sh
@@ -3,7 +3,6 @@ sudo apt --assume-yes install zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
@@ -15,5 +14,4 @@ fc-cache -f -v
 
 
 cp ./zshrc ~/.zshrc
-cp ./p10k.zsh ~/.p10k.zsh
 sudo chsh -s $(which zsh) $(whoami)
