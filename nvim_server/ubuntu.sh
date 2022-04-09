b30d9ef6567f1b006b8b0fb5fd00ebb828b3c22e
youzark <gacforstudy@gmail.com>
2022-04-09 10:59:43 +0800

    add zsh config and modify vim config

--- nvim_server/ubuntu.sh
+++ nvim_server/ubuntu.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 #set up after buildEssential
-sudo apt-get --assume-yes install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+sudo apt-get --assume-yes install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxy
+gen
 git clone https://github.com/neovim/neovim
-cd neovim && make 
-sudo make install 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-cp ./nvim ~/.config/nvim
-sudo apt-get --assume-yes nodejs
+cd neovim && make
+sudo make install
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+if [ -d "~/.config" ]; then
+        cp -r  ~/serverConfig/nvim_server/nvim ~/.config/nvim
+else
+        mkdir ~/.config
+        cp -r  ~/serverConfig/nvim_server/nvim ~/.config/nvim
+fi
+
+curl -sL install-node.vercel.app/lts > installNodejs.sh
+chmod +x installNodejs.sh
+./installNodejs.sh
+nvim -c "PackerSync"
---
+++ zsh_server/ubuntu.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+sudo apt --assume-yes install zsh
+chsh -s $(which zsh)
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cp ./zshrc ~/.zshrc
