d7cc13e30b6e6f7f549b371d67e2845e92c1bfed
youzark <gacforstudy@gmail.com>
2022-04-09 14:40:29 +0800

    modify zsh config

--- zsh_server/ubuntu.sh
+++ zsh_server/ubuntu.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 sudo apt --assume-yes install zsh
-chsh -s $(which zsh)
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 cp ./zshrc ~/.zshrc
+cp ./p10k.zsh ~/.p10k.zsh
+sudo chsh -s $(which zsh) $(whoami)
