92e1d864ab72dc30a0f7ee17ddbfd1547aa237a3
youzark <gacforstudy@gmail.com>
2022-04-09 18:24:19 +0800

    edit the pyenv setting for nvim

--- pyenv_server/ubuntu_reboot.sh
+++ pyenv_server/ubuntu_reboot.sh
@@ -3,4 +3,8 @@ pyenv install 3.9.9
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 git clone https://github.com/alefpereira/pyenv-pyright.git $(pyenv root)/plugins/pyenv-pyright
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+
+pyenv virtualenv 3.9.9 nvim
+~/.pyenv/versions/nvim/bin/pip pynvim
+
 exec "$SHELL"
