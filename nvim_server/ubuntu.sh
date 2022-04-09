6b196f54f5d2569db0131cb85554b2ad706d191c
youzark <gacforstudy@gmail.com>
2022-04-09 14:14:29 +0800

    modify vim config

--- nvim_server/ubuntu.sh
+++ nvim_server/ubuntu.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 #set up after buildEssential
-sudo apt-get --assume-yes install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxy
-gen
+sudo apt-get --assume-yes install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
 git clone https://github.com/neovim/neovim
 cd neovim && make
 sudo make install
@@ -16,5 +15,4 @@ fi
 
 curl -sL install-node.vercel.app/lts > installNodejs.sh
 chmod +x installNodejs.sh
-./installNodejs.sh
-nvim -c "PackerSync"
+sudo ./installNodejs.sh
