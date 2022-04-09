d474597ab0df0ff9ad8657226294d2fb7d5ae17a
youzark <gacforstudy@gmail.com>
2022-04-09 18:18:34 +0800

    make sure pyenv will work

--- buildEssentials/ubuntu.sh
+++ buildEssentials/ubuntu.sh
@@ -1,3 +1,3 @@
+sudo apt-get --assume-yes install -y make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
 sudo apt-get update
 sudo apt-get --assume-yes install build-essential
-sudo apt-get install git
