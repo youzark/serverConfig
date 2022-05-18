c3631e9c813523dff2c69f4f63ef1c5908ac595a
youzark <gacforstudy@gmail.com>
2022-05-18 15:08:20 +0800

    config ai environment

--- aiBuild.sh
+++ aiBuild.sh
@@ -5,3 +5,7 @@ sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/
 sudo apt-get update
 sudo apt-get -y install cuda
 
+# pytorch tensorboard huggingface
+pyenv virtualenv 3.9.9 ai
+~/.pyenv/versions/ai/bin/pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+~/.pyenv/versions/ai/bin/pip3 install tensorboard transformers datasets
